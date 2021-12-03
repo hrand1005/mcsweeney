d8d08bbb9ac29eb915fdf4ec9a4502b19c42f173
Herbie <hjrand1005@gmail.com>
2021-12-02 22:10:59 -0500

    ADD: getting this youtube spaghetti code barely functional

--- go.mod
+++ go.mod
@@ -4,7 +4,23 @@ go 1.17
 
 require (
 	github.com/nicklaw5/helix v1.25.0
+	golang.org/x/net v0.0.0-20211201190559-0a0e4e1bb54c
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+	google.golang.org/api v0.30.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
-require github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
+require (
+	cloud.google.com/go v0.65.0 // indirect
+	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
+	go.opencensus.io v0.22.4 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
+	google.golang.org/grpc v1.31.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
--- mcsweeney.go
+++ mcsweeney.go
@@ -1,35 +1,42 @@
 package main
 
+/* TODO:
+- Consistent error handling
+- Testing
+- Architecture
+    - Think about params, pointers, interfaces, etc.
+- Goroutines
+*/
 
+//TODO: maybe we don't need the entire packages?
 import (
+    "mcsweeney/auth"
+    "google.golang.org/api/youtube/v3"
 	"fmt"
 	"github.com/nicklaw5/helix"
 	"gopkg.in/yaml.v2"
-    "io"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
-    "os"
+	"os"
 	"os/exec"
 	"strings"
-    //"sync"
-    "time"
+	//"sync"
+	"time"
 )
-
 
 const (
-	RawVidsDir = "tmp/raw"
-    ProcessedVidsDir = "tmp/processed"
+	RawVidsDir       = "tmp/raw"
+	ProcessedVidsDir = "tmp/processed"
 )
-
 
 type context struct {
 	ClientID string `yaml:"clientID"`
 	Token    string `yaml:"token"`
-    GameID string `yaml:"gameID"` 
-    First int `yaml:"first"`
+	GameID   string `yaml:"gameID"`
+	First    int    `yaml:"first"`
 }
-
 
 func main() {
 	// Get context from yaml file
@@ -42,30 +49,29 @@ func main() {
 	}
 
 	// Remember, this is a strategy, so it will be more like s.GetContent()
-    clips, err := getClips(c.ClientID, c.Token, c.GameID, c.First)
+	clips, err := getClips(c.ClientID, c.Token, c.GameID, c.First)
 	if err != nil || len(clips) == 0 {
 		fmt.Println("Couldn't get content.")
 		log.Fatal(err)
 	}
-    
-	//s.EditContent()
-    editClipsTimer := clipFuncTimer(editClips)
-    err = editClipsTimer(clips)
-    if err != nil {
-        fmt.Printf("Couldn't some clips: %v\n", err)
-    }
 
-    //s.CompileContent()
-    err = compileClips()
-    if err != nil {
-        fmt.Printf("Couldn't compile clips: %v\n", err)
-    }
+	//s.EditContent()
+	editClipsTimer := clipFuncTimer(editClips)
+	err = editClipsTimer(clips)
+	if err != nil {
+		fmt.Printf("Couldn't some clips: %v\n", err)
+	}
+
+	//s.CompileContent()
+	err = compileClips()
+	if err != nil {
+		fmt.Printf("Couldn't compile clips: %v\n", err)
+	}
 	//s.CompileContent()
 	//s.ShareContent()
 
 	return
 }
-
 
 // It may be appropriate to get more information than just a token
 func loadContext(path string, c *context) error {
@@ -82,7 +88,6 @@ func loadContext(path string, c *context) error {
 	return nil
 }
 
-
 func getClips(clientID string, token string, gameID string, first int) ([]helix.Clip, error) {
 	client, err := helix.NewClient(&helix.Options{
 		ClientID: clientID,
@@ -97,10 +102,10 @@ func getClips(clientID string, token string, gameID string, first int) ([]helix.
 	// Define query for clips
 	clipParams := &helix.ClipsParams{
 		GameID: gameID,
-        First: first,
+		First:  first,
 	}
 
-    // Execute query for clips, TODO: more error checking here?
+	// Execute query for clips, TODO: more error checking here?
 	twitchResp, err := client.GetClips(clipParams)
 	if err != nil {
 		return nil, err
@@ -114,39 +119,37 @@ func getClips(clientID string, token string, gameID string, first int) ([]helix.
 	return twitchResp.Data.Clips, nil
 }
 
-
 func downloadNewClips(manyClips []helix.Clip) error {
-    start := time.Now()
-    fmt.Println("Download start...")
+	start := time.Now()
+	fmt.Println("Download start...")
 
-    //var wg sync.WaitGroup
+	//var wg sync.WaitGroup
 
-    // TODO: spawn goroutines for each download
-    // NOTE: Preliminary testing indicates not much of a difference around 14
-    // clips downloaded using this commented out method. 
-    fmt.Printf("Trying to download %v clips.\n", len(manyClips))
+	// TODO: spawn goroutines for each download
+	// NOTE: Preliminary testing indicates not much of a difference around 14
+	// clips downloaded using this commented out method.
+	fmt.Printf("Trying to download %v clips.\n", len(manyClips))
 	for _, v := range manyClips {
 		// TODO: Verify clip here
-        //wg.Add(1)
-        //v := v
+		//wg.Add(1)
+		//v := v
 		fmt.Println("Attempting to download a clip...")
-        //go func() {
-            //defer wg.Done()
-        err := downloadClip(&v)
-        if err != nil {
-            fmt.Println("Failed to download a clip: ", err)
-        }
-        //}()
+		//go func() {
+		//defer wg.Done()
+		err := downloadClip(&v)
+		if err != nil {
+			fmt.Println("Failed to download a clip: ", err)
+		}
+		//}()
 	}
 
-    //wg.Wait()
-    finish := time.Now()
-    elapsed := finish.Sub(start)
-    fmt.Printf("finished in %v\n", elapsed)
+	//wg.Wait()
+	finish := time.Now()
+	elapsed := finish.Sub(start)
+	fmt.Printf("finished in %v\n", elapsed)
 
 	return nil
 }
-
 
 func downloadClip(clip *helix.Clip) error {
 	thumbURL := clip.ThumbnailURL
@@ -154,25 +157,24 @@ func downloadClip(clip *helix.Clip) error {
 	fmt.Println("MP4 URL: ", mp4URL)
 
 	resp, err := http.Get(mp4URL)
+	defer resp.Body.Close()
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
 
 	filename := strings.SplitN(mp4URL, "twitch.tv", 2)[1]
 	outFile := RawVidsDir + filename
 
 	out, err := os.Create(outFile)
+	defer out.Close()
 	if err != nil {
 		return err
 	}
-	defer out.Close()
 
 	_, err = io.Copy(out, resp.Body)
 
 	return err
 }
-
 
 //TODO: get rid of this
 func getClipPath(clip *helix.Clip) string {
@@ -182,54 +184,103 @@ func getClipPath(clip *helix.Clip) string {
 
 	return filename
 }
-    
 
 // TODO: replace this with a ffmpeg library dear god
 // TODO: goroutines!
 func editClips(clips []helix.Clip) error {
-    f, err := os.Create("clips.txt")
-    if err != nil {
-        return err
-    }
-    defer f.Close()
+	f, err := os.Create("clips.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-    for _, v := range clips {
-        overlayText := fmt.Sprintf("%s\n%s", v.Title, v.BroadcasterName)
-        fmt.Printf("Overlay text: %s\n", overlayText)
-        filename := getClipPath(&v)
-        rawPath := RawVidsDir + filename
-        overlayArg := fmt.Sprintf(`drawtext=fontfile=/usr/share/fonts/TTF/DejaVuSans.ttf:text='%s':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=0:y=0`, overlayText)
-        processedPath := ProcessedVidsDir + filename
-        cmdName := "ffmpeg"
-        args := []string{"-i", rawPath, "-vf", overlayArg, "-codec:a", "copy", processedPath}
-        ffmpegCmd := exec.Command(cmdName, args...)
-        err := ffmpegCmd.Run()
-        if err != nil {
-            fmt.Printf("Failed to execute ffmpeg cmd: %v\n", err)
-        }
+	for _, v := range clips {
+		overlayText := fmt.Sprintf("%s\n%s", v.Title, v.BroadcasterName)
+		fmt.Printf("Overlay text: %s\n", overlayText)
+		filename := getClipPath(&v)
+		rawPath := RawVidsDir + filename
+		overlayArg := fmt.Sprintf(`drawtext=fontfile=/usr/share/fonts/TTF/DejaVuSans.ttf:text='%s':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:boxborderw=5:x=0:y=0`, overlayText)
+		processedPath := ProcessedVidsDir + filename
+		cmdName := "ffmpeg"
+		args := []string{"-i", rawPath, "-vf", overlayArg, "-codec:a", "copy", processedPath}
+		ffmpegCmd := exec.Command(cmdName, args...)
+		err := ffmpegCmd.Run()
+		if err != nil {
+			fmt.Printf("Failed to execute ffmpeg cmd: %v\n", err)
+		}
 
+		w := fmt.Sprintf("file '%s'\n", processedPath)
+		_, err = f.WriteString(w)
+		if err != nil {
+			return err
+		}
+	}
 
-        w := fmt.Sprintf("file '%s'\n", processedPath)
-        _, err = f.WriteString(w)
-        if err != nil {
-            return err
-        }
-    }
-    
-    return nil
+	return nil
 }
 
-
 func compileClips() error {
-    cmdName := "ffmpeg"
-    args := []string{"-f", "concat", "-safe", "0", "-i", "clips.txt", "compiled-vid.mp4"}
-    cmd := exec.Command(cmdName, args...)
-    err := cmd.Run()
-    if err != nil {
-        fmt.Printf("Failed to execute ffmpeg cmd: %v\n", err)
-    }
+	cmdName := "ffmpeg"
+	args := []string{"-f", "concat", "-safe", "0", "-i", "clips.txt", "compiled-vid.mp4"}
+	cmd := exec.Command(cmdName, args...)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Failed to execute ffmpeg cmd: %v\n", err)
+	}
 
-    return nil
+	return nil
+}
+
+type uploadArgs struct {
+	Filename    string
+	Title       string
+	Description string
+	Category    string
+	Keywords    string
+	Privacy     string
+}
+
+func uploadVideo(args uploadArgs) (*youtube.Video, error) {
+	if args.Filename == "" {
+		return nil, fmt.Errorf("cannot upload nil file")
+	}
+	//TODO: perform checks on the inputs
+
+	// TODO: figure out this
+	client := auth.GetClient(youtube.YoutubeReadonlyScope)
+
+	service, err := youtube.New(client)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't create youtube service: %v", err)
+	}
+
+	upload := &youtube.Video{
+		Snippet: &youtube.VideoSnippet{
+			Title:       args.Title,
+			Description: args.Description,
+			CategoryId:  args.Category,
+			Tags:        strings.Split(args.Keywords, ","),
+		},
+		Status: &youtube.VideoStatus{PrivacyStatus: args.Privacy},
+	}
+
+    insertArgs := []string{"snippet","status"}
+	call := service.Videos.Insert(insertArgs, upload)
+
+	file, err := os.Open(args.Filename)
+	defer file.Close()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't open file: %s, %v", args.Filename, err)
+	}
+
+	response, err := call.Media(file).Do()
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't upload file: %v", err)
+	}
+
+	fmt.Printf("%s uploaded successfully!\nTitle: %s\n", args.Filename, args.Title)
+
+	return response, nil
 }
 
 // Consider this in the final version
@@ -243,16 +294,15 @@ func main(){
 }
 */
 
-
 // some of that experimental stuff
 type clipFunc func([]helix.Clip) error
 
 func clipFuncTimer(f clipFunc) clipFunc {
-    return func(c []helix.Clip) error {
-        defer func(t time.Time) {
-            fmt.Printf("clipFunc elapsed in %v\n", time.Since(t))
-        }(time.Now())
+	return func(c []helix.Clip) error {
+		defer func(t time.Time) {
+			fmt.Printf("clipFunc elapsed in %v\n", time.Since(t))
+		}(time.Now())
 
-        return f(c)
-    }
+		return f(c)
+	}
 }
