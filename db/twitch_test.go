81f75b2915250af0c82043d8ac7291a893c0b7c1
Herbie <hjrand1005@gmail.com>
2021-12-09 21:02:55 -0500

    ADD: test twitch db Exists()

--- db/twitch_test.go
+++ db/twitch_test.go
@@ -8,27 +8,35 @@ import (
 	"testing"
 )
 
+const (
+	selectClip = `SELECT url FROM twitch WHERE url=?`
+	testDB     = "test-twitch.db"
+	testURL    = "www.herb.com"
+)
+
 func TestInsert(t *testing.T) {
-	db, err := NewTwitchDB("test-insert.db")
-	defer os.Remove("test-insert.db")
+	// init test db
+	db, err := NewTwitchDB(testDB)
+	defer os.Remove(testDB)
 	assert.Assert(t, err == nil)
-	err = db.Insert("www.herb.com")
+
+	// insert test url
+	err = db.Insert(testURL)
 	assert.Assert(t, err == nil)
-	selectClip := `SELECT url FROM twitch WHERE url=?`
 
 	testCases := []struct {
 		name    string
-		wantUrl string
+		wantURL string
 		wantErr error
 	}{
 		{
-			name:    "Success: inserted clip 'www.herb.com' retrieved",
-			wantUrl: "www.herb.com",
+			name:    "Success: inserted clip retrieved",
+			wantURL: testURL,
 			wantErr: nil,
 		},
 		{
-			name:    "Failure: clip 'www.herb2.com' not found",
-			wantUrl: "",
+			name:    "Failure: clip not found",
+			wantURL: "",
 			wantErr: sql.ErrNoRows,
 		},
 	}
@@ -37,9 +45,46 @@ func TestInsert(t *testing.T) {
 		statement, err := db.dbHandle.Prepare(selectClip)
 		assert.Assert(t, err == nil)
 
-		var gotUrl string
-		gotErr := statement.QueryRow(tc.wantUrl).Scan(&gotUrl)
-		assert.Equal(t, tc.wantUrl, gotUrl)
+		var gotURL string
+		gotErr := statement.QueryRow(tc.wantURL).Scan(&gotURL)
+		assert.Equal(t, tc.wantURL, gotURL)
+		assert.Equal(t, tc.wantErr, gotErr)
+	}
+}
+
+func TestExists(t *testing.T) {
+	// init test db
+	db, err := NewTwitchDB(testDB)
+	defer os.Remove(testDB)
+	assert.Assert(t, err == nil)
+
+	//insert test url
+	err = db.Insert(testURL)
+	assert.Assert(t, err == nil)
+
+	testCases := []struct {
+		name       string
+		url        string
+		wantExists bool
+		wantErr    error
+	}{
+		{
+			name:       "Success: clip exists in db",
+			url:        testURL,
+			wantExists: true,
+			wantErr:    nil,
+		},
+		{
+			name:       "Success: clip doesn't exist in db",
+			url:        "",
+			wantExists: false,
+			wantErr:    nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		gotExists, gotErr := db.Exists(tc.url)
+		assert.Equal(t, tc.wantExists, gotExists)
 		assert.Equal(t, tc.wantErr, gotErr)
 	}
 }
