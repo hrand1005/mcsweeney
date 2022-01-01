a3c8a23f03b78354a68e6ed4ca0a77b91a5014cd
Herbie <hjrand1005@gmail.com>
2022-01-01 17:11:40 -0500

    ADD: overlay generator VisitClip implementation, as well as access methods.

--- content/overlay.go
+++ content/overlay.go
@@ -1,36 +1,122 @@
 package content
 
 import (
-
+	"fmt"
+	"strings"
 )
 
 // OverlayGenerator generates video overlays for the video components it visits.
 type OverlayGenerator struct {
-    cursor float64
-    overlay string
+	Font       string
+	Background string
+	args       []string
+	cursor     float64
+	overlay    string
 }
 
-// String returns a string of the generated overlay. The overlay is the 
-// aggregate of all visited elements, also reflecting visit order.
-func (o *OverlayGenerator) String() string {
-    return o.overlay
-}
-
-// VisitClip implements the visitor interface for OverlayGenerator. 
-func (o *OverlayGenerator) VisitClip(c *Clip) {
-    return
-}
-
-// VisitIntro implements the visitor interface for OverlayGenerator. 
+// VisitIntro implements the visitor interface for OverlayGenerator.
 // Increments internal cursor, but does not alter the overlay string.
 func (o *OverlayGenerator) VisitIntro(i *Intro) {
 	o.cursor += i.Duration
 	return
 }
 
-// VisitOutro implements the visitor interface for OverlayGenerator. 
+// VisitOutro implements the visitor interface for OverlayGenerator.
 // Increments internal cursor, but does not alter the overlay string.
 func (o *OverlayGenerator) VisitOutro(u *Outro) {
 	o.cursor += u.Duration
 	return
+}
+
+// These constants are restrictive, but simplify the client's interface to
+// create overlays.
+// TODO: Make these values smarter and/or configurable
+// TODO: Create default values, allow override
+const (
+	OverlayDuration float64 = 3
+	SlideSpeed      float64 = 2000
+	XPosition       int     = 20
+	YPosition       int     = 500
+
+	FontColor string  = "ffffff"
+	FontSize  int     = 26
+	TextFade  float64 = 0.5
+	TextDelay float64 = 0.5
+)
+
+// VisitClip implements the visitor interface for OverlayGenerator. It generates
+// an overlay for the clip using the clips Title and Broadcaster fields, as well
+// as a number of configurable options for the OverlayGenerator instance.
+func (o *OverlayGenerator) VisitClip(c *Clip) {
+	bgOverlay := o.createOverlayBackground(c)
+	textOverlay := o.createOverlayText(c)
+
+	// update input args, overlay string, and cursor
+	o.args = append(o.args, "-i", o.Background)
+	o.overlay += bgOverlay + textOverlay + ","
+	o.cursor += c.Duration
+	return
+}
+
+// String returns a string of the generated overlay. The overlay is the
+// aggregate of all visited elements, also reflecting visit order.
+func (o *OverlayGenerator) String() string {
+	if o.overlay == "" {
+		return ""
+	}
+	var overlayString string
+	for _, v := range o.args {
+		overlayString += v + " "
+	}
+	return overlayString + "-filter_complex " + strings.TrimRight(o.overlay, ",")
+}
+
+// Slice returns a slice representation of the generated overlay.
+func (o *OverlayGenerator) Slice() []string {
+	if len(o.args) == 0 {
+		return nil
+	}
+	return append(o.args, "-filter_complex", strings.TrimRight(o.overlay, ","))
+}
+
+func (o *OverlayGenerator) createOverlayBackground(c *Clip) string {
+	// determine the length of the overlay's background, multiply by arbitrary
+	// size coefficient (16), plus x offset to create margins for text
+	bgLen := float64(max(len(c.Title), len(c.Broadcaster)))*16.0 + 3.5*float64(XPosition)
+
+	// determine the duration of the background's slide animation
+	sDur := bgLen / SlideSpeed
+
+	// generate strings to animate the background x and y position over time
+	yString := fmt.Sprintf(`y=%v`, YPosition-30)
+	xString := fmt.Sprintf(`x='if(lt(t,%f),NAN,if(lt(t,%f),-w+(t-%f)*%f,if(lt(t,%f),-w+%f,-w+%f-(t-%f)*%f)))'`, o.cursor, o.cursor+sDur, o.cursor, SlideSpeed, o.cursor+sDur+OverlayDuration, bgLen, bgLen, o.cursor+sDur+OverlayDuration, SlideSpeed)
+
+	return fmt.Sprintf(`overlay=%s:%s,`, xString, yString)
+}
+
+func (o *OverlayGenerator) createOverlayText(c *Clip) string {
+	fontString := fmt.Sprintf(`drawtext=fontfile=%s`, o.Font)
+	textString := fmt.Sprintf("text=%s\n%s", escapeText(c.Title), escapeText(c.Broadcaster))
+	sizeString := fmt.Sprintf(`fontsize=%v`, FontSize)
+	colorString := fmt.Sprintf(`fontcolor=%s`, FontColor)
+
+	// generate string to fade text in over background with a delay
+	fadeString := fmt.Sprintf(`alpha='if(lt(t,%f),0,if(lt(t,%f),(t-%f)/1,if(lt(t,%f),1,if(lt(t,%f),(1-(t-%f))/1,0))))'`, o.cursor+TextDelay, o.cursor+TextDelay+TextFade, o.cursor+TextDelay, o.cursor+OverlayDuration-TextDelay, o.cursor+OverlayDuration-TextDelay+TextFade, o.cursor+OverlayDuration-TextDelay)
+	xString := fmt.Sprintf(`x=%v`, XPosition)
+	yString := fmt.Sprintf(`y=%v`, YPosition)
+
+	return fmt.Sprintf(`%s:%s:%s:%s:%s:%s:%s`, fontString, textString, sizeString, colorString, fadeString, xString, yString)
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func escapeText(s string) string {
+	s = strings.ReplaceAll(s, `'`, `\\\'`)
+	s = strings.ReplaceAll(s, `:`, `\\\:`)
+	return strings.ReplaceAll(s, `,`, `\\\,`)
 }
--- content/overlay_test.go
+++ content/overlay_test.go
@@ -1,39 +1,39 @@
 package content_test
 
 import (
-    "mcsweeney/content"
-    "testing"
+	"mcsweeney/content"
+	"testing"
 )
 
 // TestOverlayGeneratorVisitIntro calls OverlayGenerator.VisitIntro and
 // provides an Intro element, checking that the OverlayGenerator's String()
-// method returns the expected string representation. 
+// method returns the expected string representation.
 // TODO: Decide what overlays should be generated for intros
 // Currently VisitIntro does not change OverlayGenerator's string
 // representation. However, it should still have effects on OverlayGenerator's
-// internal state, such as incrementing the OverlayGenerator's internal cursor. 
+// internal state, such as incrementing the OverlayGenerator's internal cursor.
 func TestOverlayGeneratorVisitIntro(t *testing.T) {
-    tests := []struct{
-        name string
-        intro *content.Intro
-        visitor *content.OverlayGenerator
-        want string
-    }{
-        {
-            name: "Nominal overlay for intro, empty string.",
-            intro: &content.Intro{
-                Duration: 1.0,
-            },
-            visitor: &content.OverlayGenerator{},
-            want: "",
-        },
-    }
-    for _, tc := range tests {
+	tests := []struct {
+		name    string
+		intro   *content.Intro
+		visitor *content.OverlayGenerator
+		want    string
+	}{
+		{
+			name: "Nominal overlay for intro, empty string.",
+			intro: &content.Intro{
+				Duration: 1.0,
+			},
+			visitor: &content.OverlayGenerator{},
+			want:    "",
+		},
+	}
+	for _, tc := range tests {
 		tc.visitor.VisitIntro(tc.intro)
 		if tc.visitor.String() != tc.want {
 			t.Fatalf("Got: %s\nWanted: %s", tc.visitor.String(), tc.want)
 		}
-    }
+	}
 }
 
 // TestOverlayGeneratorVisitOutro calls OverlayGenerator.VisitOutro and
@@ -42,37 +42,68 @@ func TestOverlayGeneratorVisitIntro(t *testing.T) {
 // TODO: Decide what overlays should be generated for intros
 // Currently VisitIntro does not change OverlayGenerator's string
 // representation. However, it should still have effects on OverlayGenerator's
-// internal state, such as incrementing the OverlayGenerator's internal cursor. 
+// internal state, such as incrementing the OverlayGenerator's internal cursor.
 func TestOverlayGeneratorVisitOutro(t *testing.T) {
-    tests := []struct{
-        name string
-        outro *content.Outro
-        visitor *content.OverlayGenerator
-        want string
-    }{
-        {
-            name: "Nominal overlay for outro, empty string.",
-            outro: &content.Outro{
-                Duration: 1.0,
-            },
-            visitor: &content.OverlayGenerator{},
-            want: "",
-        },
-    }
-    for _, tc := range tests {
+	tests := []struct {
+		name    string
+		outro   *content.Outro
+		visitor *content.OverlayGenerator
+		want    string
+	}{
+		{
+			name: "Nominal overlay for outro, empty string.",
+			outro: &content.Outro{
+				Duration: 1.0,
+			},
+			visitor: &content.OverlayGenerator{},
+			want:    "",
+		},
+	}
+	for _, tc := range tests {
 		tc.visitor.VisitOutro(tc.outro)
 		if tc.visitor.String() != tc.want {
 			t.Fatalf("Got: %s\nWanted: %s", tc.visitor.String(), tc.want)
 		}
-    }
+	}
 }
 
 // TestOverlayGeneratorVisitClip calls OverlayGenerator.VisitClip and
 // provides an Clip element, checking that the OverlayGenerator's String()
 // method returns the expected string representation.
-//func TestOverlayGeneratorVisitClip(t *testing.T) {
-
-//}
+func TestOverlayGeneratorVisitClip(t *testing.T) {
+	tests := []struct {
+		name    string
+		clip    *content.Clip
+		visitor *content.OverlayGenerator
+		want    string
+	}{
+		{
+			name: "Overlay clip with background set in OverlayGenerator.",
+			clip: &content.Clip{
+				Title:       "TestTitle",
+				Broadcaster: "TestBroadcaster",
+				Duration:    1.0,
+			},
+			visitor: &content.OverlayGenerator{
+				Background: "/path/to/Background.png",
+				Font:       "/path/to/Font.ttf",
+			},
+			want: "-i /path/to/Background.png -filter_complex overlay=x='if(lt(t,0.000000)," +
+				"NAN,if(lt(t,0.155000),-w+(t-0.000000)*2000.000000,if(lt(t,3.155000)," +
+				"-w+310.000000,-w+310.000000-(t-3.155000)*2000.000000)))':y=470," +
+				"drawtext=fontfile=/path/to/Font.ttf:text=TestTitle\nTestBroadcaster:" +
+				"fontsize=26:fontcolor=ffffff:alpha='if(lt(t,0.500000),0,if(lt(t,1.000000)," +
+				"(t-0.500000)/1,if(lt(t,2.500000),1,if(lt(t,3.000000),(1-(t-2.500000))/1," +
+				"0))))':x=20:y=500",
+		},
+	}
+	for _, tc := range tests {
+		tc.visitor.VisitClip(tc.clip)
+		if tc.visitor.String() != tc.want {
+			t.Fatalf("Got: %s\nWanted: %s", tc.visitor.String(), tc.want)
+		}
+	}
+}
 
 // TestOverlayGeneratorVisitMany calls multiple visit methods on
 // OverlayGenerator in sequence and checks that the resulting string returned by
